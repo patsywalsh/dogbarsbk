74dcf77dca9ea7669ba5f164c3ae4608f3235834
PATSY WALSH^C <PATSYPATSYPATSY@gmail.com^C>
2014-07-10 20:31:26 -0400

    updated

---
+++ db/seeds.rb
@@ -0,0 +1,24 @@
+User.create!([
+  {email: "pww@pww.com", encrypted_password: "$2a$10$Jflb/fZVD.ITt2pKhN9Rmuy3nnO550cwBMroWj6X7mZIBTpDgXpmm", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 2, current_sign_in_at: "2014-07-10 00:48:56", last_sign_in_at: "2014-07-09 23:01:16", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1"}
+])
+Place.create!([
+  {name: "Luckydog", description: "Cool Backyard", neighborhood: "", rating: 5, latitude: 40.713484, longitude: -73.9616836, address: "303 Bedford Avenue, Brooklyn NY", location: nil},
+  {name: "Bar Great Harry", description: "#dogsofbargreatharry", neighborhood: "", rating: 4, latitude: 40.6824203, longitude: -73.99355280000002, address: "280 Smith St, Brooklyn NY", location: nil},
+  {name: "Randolph Brooklyn", description: "Cocktails", neighborhood: "", rating: 4, latitude: 40.7120244, longitude: -73.9633429, address: "104 S 4th Street, Brooklyn NY", location: nil},
+  {name: "The Levee", description: "Free cheeseballs.", neighborhood: "", rating: 4, latitude: 40.7190055, longitude: -73.95838599999999, address: "103 Berry St, Brooklyn NY", location: nil},
+  {name: "Brooklyn Ale House", description: "Great outdoor space.", neighborhood: "", rating: 4, latitude: 40.6761542, longitude: -73.9833812, address: "249 4th Ave Brooklyn, NY 11215", location: nil},
+  {name: "The Gate", description: "Pub", neighborhood: "", rating: 3, latitude: 40.672564, longitude: -73.98317, address: "321 5th Ave Brooklyn, NY 11215", location: nil},
+  {name: "duckduck", description: "divedive", neighborhood: "", rating: 3, latitude: 40.70738739999999, longitude: -73.9427932, address: "161 Montrose Ave Brooklyn, NY 11206", location: nil},
+  {name: "Pine Box Rock Shop", description: "Good happy hour", neighborhood: "", rating: 4, latitude: 40.70524899999999, longitude: -73.932667, address: "12 Grattan St Brooklyn, NY 11206", location: nil},
+  {name: "South 4th Bar & Café", description: "Fresh arnold palmers", neighborhood: "", rating: 5, latitude: 40.7122598, longitude: -73.9643924, address: "90 S 4th St Brooklyn, NY 11249", location: nil},
+  {name: "Skinny Dennis", description: "Peanuts", neighborhood: "", rating: 4, latitude: 40.715863, longitude: -73.96216799999999, address: "152 Metropolitan Ave Brooklyn, NY 11249", location: nil},
+  {name: "High Dive", description: "Free popcorn.", neighborhood: "", rating: 4, latitude: 40.6748993, longitude: -73.9811769, address: "243 5th Ave Brooklyn, NY 11215", location: nil},
+  {name: "Red Hook Bait & Tackle", description: "Taxidermy...", neighborhood: "", rating: 3, latitude: 40.679139, longitude: -74.011161, address: "320 Van Brunt St Brooklyn, NY 11231", location: nil},
+  {name: "Iona", description: "Ping Pong", neighborhood: "", rating: 4, latitude: 40.71418269999999, longitude: -73.96107119999999, address: "180 Grand St Brooklyn, NY 11211", location: nil},
+  {name: "Lone Wolf", description: "Good jukebox.", neighborhood: "", rating: 4, latitude: 40.694531, longitude: -73.930426, address: "1089 Broadway Brooklyn, NY 11221", location: nil},
+  {name: "The Fulton Grand", description: "Free pretzels.", neighborhood: "", rating: 4, latitude: 40.682526, longitude: -73.96144, address: "1011 Fulton St Brooklyn, NY 11238", location: nil},
+  {name: "4th Avenue Pub", description: "Lots of beers.", neighborhood: "", rating: 4, latitude: 40.6823025, longitude: -73.9800858, address: "76 4th Ave Brooklyn, NY 11217", location: nil},
+  {name: "Pacific Standard Brooklyn", description: "Microbrews.", neighborhood: "", rating: 4, latitude: 40.6821219, longitude: -73.9802204, address: "82 4th Ave Brooklyn, NY 11217", location: nil},
+  {name: "The Owl Farm", description: "Pinball.", neighborhood: "", rating: 5, latitude: 40.669605, longitude: -73.986597, address: "297 9th St Brooklyn, NY 11215", location: nil},
+  {name: "Smooch", description: "Dogs, babies, coffee, wine.", neighborhood: "", rating: 3, latitude: 40.6898204, longitude: -73.9721979, address: "264 Carlton Ave Brooklyn, NY 11205", location: nil}
+])
